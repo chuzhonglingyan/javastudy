c2b126a15a1dd7e223b46b642c6f38b1d68c99a3
yuntian <944610721@qq.com>
2019-09-19 20:12:56 +0800

    初始化java学习项目

--- src/main/java/com/yuntian/study/day01/Day01Demo.java
+++ src/main/java/com/yuntian/study/day01/Day01Demo.java
@@ -1,9 +1,15 @@
 package com.yuntian.study.day01;
 
 /**
+ * @author Administrator
  * @Auther: yuntian
  * @Date: 2019/9/18 0018 21:26
  * @Description: 第一天学习
  */
 public class Day01Demo {
+
+
+    public static  void main(String[] args){
+        System.out.println("启动。。。");
+    }
 }
